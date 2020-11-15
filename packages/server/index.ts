63c163c0ace4d019c9ca9856aad62da27ad10af4
Konstantinos Koukourakis <konstantinoskouk90@gmail.com>
2020-11-15 17:10:28 +0000

    #ATLAS-1: Serving next app from koa be.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,6 +3,7 @@ import Router from "@koa/router";
 import app from "../client/index";
 
 const port = process.env.PORT && parseInt(process.env.PORT, 10) || 3000;
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = new Application();
@@ -15,6 +16,11 @@ app.prepare().then(() => {
 
   router.get('/b', async (ctx) => {
     await app.render(ctx.req, ctx.res, '/b', ctx.query);
+    ctx.respond = false;
+  });
+
+  router.all('(.*)', async (ctx) => {
+    await handle(ctx.req, ctx.res);
     ctx.respond = false;
   });
 
